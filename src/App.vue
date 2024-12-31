8c248169d8e010e0338f7b6c5b53b15b233b2d4d
audricfullhardt <fullhardtaudric@gmail.com>
2025-01-01 00:09:34 +0100

    click more than 5 times

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <p>Count: {{ count }}</p>
     <div v-if="count > 5">Bravo vous avez cliqué plus de 5 fois</div>
+    <div v-else>Vous n'avez pas cliqué plus de 5 fois</div>
     <button @click="count++">Increment</button>
     <button @click="count--">Decrement</button>
 </template>
